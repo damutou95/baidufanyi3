c5df9c183f8daa7edd69631c6bd7b95bb807c7ae
damutou95 <xzcjiangxi@163.com>
2019-01-29 15:32:19 +0800

    #CONCURRENT_REQUESTS = 32

--- baidufanyi/settings.py
+++ baidufanyi/settings.py
@@ -35,7 +35,7 @@ HEADERS = {
 }
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+#CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
